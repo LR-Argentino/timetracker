16f3f1d24026300e5cc34f2458240cf3deba93d6
argentino_la <luca.argentino@outlook.com>
2023-01-01 15:57:31 +0100

    Complete TimeBookingResourceTest.java

--- src/test/java/org/blackbird/TimeBookingResourceTest.java
+++ src/test/java/org/blackbird/TimeBookingResourceTest.java
@@ -3,6 +3,7 @@ package org.blackbird;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.blackbird.model.Employee;
+import org.blackbird.model.TimeBooking;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
@@ -40,12 +41,36 @@ public class TimeBookingResourceTest {
 
     @Test
     public void testUpdateTimeBooking() {
-
+        createSomeEmployees();
+        var booking = given()
+                .body("{\"startDate\":\"2022-01-22T06:07:08.644\",\"endDate\":\"2022-01-22T12:30:08.644\"}")
+                .contentType(ContentType.JSON)
+                .when().post("/api/v1/bookings")
+                .as(TimeBooking.class);
+        booking.setEndDate(LocalDateTime.of(2022, 10, 23, 10, 20, 35));
+        given()
+                .body(booking)
+                .contentType(ContentType.JSON)
+                .when().put("/api/v1/bookings/" + booking.id)
+                .then()
+                .statusCode(200)
+                .body("endDate", is(LocalDateTime.of(2022, 10, 23, 10, 20, 35).toString()));
     }
 
     @Test
     public void deleteTimeBooking() {
+        createSomeEmployees();
+        var booking = given()
+                .body("{\"startDate\":\"2022-01-22T06:07:08.644\",\"endDate\":\"2022-01-22T12:30:08.644\"}")
+                .contentType(ContentType.JSON)
+                .when().post("/api/v1/bookings")
+                .as(TimeBooking.class);
 
+        given()
+                .when().delete("/api/v1/bookings/" + booking.id)
+                .then()
+                .statusCode(204);
+        assertThat(TimeBooking.findById(booking.id).await().indefinitely(), nullValue());
     }
 
     public void createSomeEmployees() {
